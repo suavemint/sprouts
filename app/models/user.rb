55c90f16420db9fda5965ebed39e788b7bf289f2
James Orcutt <ej.orcutt@gmail.com>
2017-12-14 20:05:06 -0500

    Adds #create_transaction! for rudimentary handling of amount value to (eventually) Dwolla methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :transactions
+
+  def create_transaction! amount
+    self.transactions << Transaction.new( amount: amount )
+  end
 end
